a4ef1d8d4b48866d94d2ebc239072ea0f0978d78
Alexander_Ovchar <azalexxx02@gmail.com>
2024-01-24 02:36:52 +0200

    reset pass

--- src/pages/auth/Reset.jsx
+++ src/pages/auth/Reset.jsx
@@ -1,14 +1,40 @@
+import { sendPasswordResetEmail } from "firebase/auth";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Loader } from "src/components/Loader";
+import { auth } from "src/firebase/firebase.config";
 
 export const Reset = () => {
   const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const resetPassword = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        setIsLoading(false);
+        toast.success("Check your email for reset password link");
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        toast.error(error.message);
+      });
+  };
 
   return (
     <div className="customContainer flex justify-center items-center flex-auto flex-col h-full">
+      {isLoading && <Loader />}
       <h1>Reset password</h1>
-      <form className="flex flex-col">
-        <input type="password" placeholder="password" required />
+      <form className="flex flex-col" onSubmit={resetPassword}>
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
         <button type="submit">Reset password</button>
       </form>
       <p>
